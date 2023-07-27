85bbc57591abbc7afb052b4cdc261c6e98b40e62
sami <samif3443@gmail.com>
2023-07-27 13:25:04 -0700

    Fix errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Categories from './components/Categories';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+import Categories from './Components/Categories';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
 import './App.css';
 
 function App() {
