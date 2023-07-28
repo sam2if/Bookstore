bfac40f57cb9e45c601a586c62a67b38bc177e76
sami <samif3443@gmail.com>
2023-07-28 14:47:40 -0700

    Fixe linter errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/NavBar';
 import Books from './components/BookList';
 import Categories from './components/Categories';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
